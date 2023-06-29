ee3537d83d4399134a03f8c4482f9e12416c256e
kiqneres <137958248+kiqneres@users.noreply.github.com>
2023-06-28 21:25:28 -0300

    Add files via upload

---
+++ estilo.js
@@ -0,0 +1,32 @@
+
+
+var nome = "Kaique Neres";
+var idade = "17";
+var email = "kiq01@hotmail.com";
+var telefone = "11988753371";
+var salario = "10.000,00";
+
+function mostrar(){
+document.querySelector("#nome").textContent = nome;
+document.querySelector("#idade").textContent = idade;
+document.querySelector("#email").textContent = email;
+document.querySelector("#telefone").textContent = telefone;
+document.querySelector("#salario").textContent = salario;
+}
+
+function mudarCor(){
+
+document.querySelector("#um").style.backgroundColor = "red" ;
+document.querySelector("#dois").style.backgroundColor = "pink" ;
+document.querySelector("#tres").style.backgroundColor = "blue" ;
+document.querySelector("#quatro").style.backgroundColor = "violet" ;
+document.querySelector("#cinco").style.backgroundColor = "purple" ;
+
+document.querySelector("#b1").style.backgroundColor = "red";
+document.querySelector("#b2").style.backgroundColor = "blue";
+
+
+
+
+}
+
